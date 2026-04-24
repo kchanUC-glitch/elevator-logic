c0c202756a8a5563ec41e292ae29b142a53bd5e9
kcglitch <chank2@mail.uc.edu>
2026-04-24 17:52:18 -0400

    person class

--- simulator.cpp
+++ simulator.cpp
@@ -5,7 +5,24 @@ using namespace std;
 
 const int MAX_PEOPLE_PER_ELEVATOR = 10;
 
+class person {
+    public:
+        person(int);
+
+    private: 
+        int destination;
+        int getDestination();
+};
+
 int main () {
 
     return 0;
-}
+};
+
+// implementation for class person
+person::person (int destination){
+    destination = this->destination;
+};
+int person::getDestination () {
+    return destination;
+};
