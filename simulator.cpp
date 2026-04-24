03c4adb167c71aefd6856b83f1c836ea4f17d1e9
Austin <austin.florence.72@gmail.com>
2026-04-24 19:36:31 -0400

    Added elevator class

--- simulator.cpp
+++ simulator.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 
 using namespace std;
+
+class Elevator {
+    public:
+        Elevator();
+        void addDestination(Person);
+        void display();
+        int getCurrFloor();
+        vector<int> getDestinations();
+    private:
+        int currFloor;
+        string name;
+        int currNumPeople;
+        vector<int> destinations;   
+};
 
 int main () {
 
